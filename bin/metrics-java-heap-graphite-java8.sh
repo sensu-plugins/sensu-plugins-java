7c37a04266c32fa03857e1fbed980551f35aad08
Nikoletta Kyriakidou <nk8smg@hotmail.com>
2017-02-13 02:07:37 +0000

    Support java 8 and multiple java processes  (#13)
    * Changed to support java 8
    
    * Created separated file for java 8 support
    
    * added new metric file for java 8 support
    
    * jstat metric separate file for java 8 support
    
    * Updated README to include new java 8 files
    
    * Revert space change
    
    * Replaced comma with hyphen on README file
    
    * Remove space
    
    * Removed 1.9.3 version of ruby and added 2.3.0

---
+++ bin/metrics-java-heap-graphite-java8.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+#
+# Collect metrics on your JVM and allow you to trace usage in graphite
+
+# Modified: Nikoletta Kyriakidou
+
+# You must have openjdk-8-jdk and openjdk-8-jre packages installed
+# http://openjdk.java.net/install/
+
+# Also make sure the user "sensu" can sudo without password
+
+# #RED
+while getopts 's:n:h' OPT; do
+case $OPT in
+s) SCHEME=$OPTARG;;
+n) NAME=$OPTARG;;
+h) hlp="yes";;
+esac
+done
+#usage
+HELP="
+        usage $0 [ -n value -s value -h ]
+                -n --> NAME or name of jvm process < value
+		-s --> SCHEME or server name ex. :::name::: < value
+                -h --> print this help screen
+"
+if [ "$hlp" = "yes" ]; then
+        echo "$HELP"
+        exit 0
+        fi
+
+SCHEME=${SCHEME:=0}
+NAME=${NAME:=0}
+
+#Get PIDs of JVM.
+#At this point grep for the names of the java processes running your jvm.
+PID=$(sudo jps | grep $NAME | awk '{ print $1}')
+for PID in $PIDS
+do
+  #Get heap capacity of JVM
+  TotalHeap=$(sudo jstat -gccapacity $PID  | tail -n 1 | awk '{ print ($4 + $5 + $6 + $10) / 1024 }')
+
+  #Determine amount of used heap JVM is using
+  UsedHeap=$(sudo jstat -gc $PID  | tail -n 1 | awk '{ print ($3 + $4 + $6 + $8) / 1024 }')
+
+  #Determine Old Space Utilization
+  OldGen=$(sudo jstat -gc $PID  | tail -n 1 | awk '{ print ($8) / 1024 }')
+
+  #Determine Eden Space Utilization
+  ParEden=$(sudo jstat -gc $PID  | tail -n 1 | awk '{ print ($6) / 1024 }')
+
+  #Determine Survivor Space utilization
+  ParSurv=$(sudo jstat -gc $PID  | tail -n 1 | awk '{ print ($3 + $4) / 1024 }')
+
+  #For multiple projects running we need to print the name
+	projectSize=$(printf "%s\n" $(printf "$PIDS" | wc -w))
+
+	if [ "$projectSize" -ne 1 ]; then
+		projectName=$(sudo jps | grep $PID | awk '{ print $2}' | cut -d. -f1)
+		project=$projectName
+	fi
+
+  echo "JVMs.$SCHEME.$project.Committed_Heap $TotalHeap `date '+%s'`"
+  echo "JVMs.$SCHEME.$project.Used_Heap $UsedHeap `date '+%s'`"
+  echo "JVMs.$SCHEME.$project.Eden_Util $ParEden `date '+%s'`"
+  echo "JVMs.$SCHEME.$project.Survivor_Util $ParSurv `date '+%s'`"
+  echo "JVMs.$SCHEME.$project.Old_Util $OldGen `date '+%s'`"
+done
